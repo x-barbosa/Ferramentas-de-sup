b4852c49de14df2ffbd916c2c623c19ba2866467
x-barbosa <jp51785@hotmail.com.br>
2025-11-18 15:19:43 -0300

    Create BAT_Para_Buscar_Script.bat
    Adição da bat para iniciar o FastScript facilmente.

---
+++ BAT_Para_Buscar_Script.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"C:\FASTSCRIPT\FastScript.ps1"'"
